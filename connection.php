e1741aaefd1eb8025ac756862fa06b2b5e402e49
00mayuri19 <164994767+00mayuri19@users.noreply.github.com>
2024-11-29 20:09:35 +0530

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "srv544.hstgr.io";
+$username = "u745359346_hrms_wdiaug_24";
+$password = "WDI_HRMS_aug_24_calanjiyam#2024";
+$dbname = "u745359346_hrms_wdiaug_24";
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>       
